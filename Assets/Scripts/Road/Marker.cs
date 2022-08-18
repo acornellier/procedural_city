f4a9acb2fd23bf3a4a04a0ec1be66fe8c26f0a7e
Alex Cornellier <alex@cornellier.com>
2022-08-18 18:10:31 -0400

    wip

---
+++ Assets/Scripts/Road/Marker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Marker : MonoBehaviour
+{
+    [SerializeField] bool _openForConnections;
+    [SerializeField] List<Marker> _neighbors;
+
+    void OnDrawGizmos()
+    {
+        if (Selection.activeObject != gameObject)
+            return;
+
+        foreach (var neighbor in _neighbors)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, neighbor.transform.position);
+        }
+    }
+}
---
+++ Assets/Scripts/Road/Road.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Road : MonoBehaviour
+{
+    Marker[] _carMarkers;
+
+    void Awake()
+    {
+        _carMarkers = GetComponentsInChildren<Marker>();
+    }
+    //
+    // Marker GetClosestMarkTo(Vector2 position, bool isCorner = false)
+    // {
+    //     return _carMarkers
+    //         .OrderBy(marker => Vector3.Distance(position, marker.transform.position))
+    //         .First();
+    // }
+}
